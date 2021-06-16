1c174e15a98bc3fbe5f2317df3ef80552c23272b
Evilim <evilimgabriela.eg@gmail.com>
2021-06-16 13:16:19 -0300

    Alteração e protótipo do método addProduto

--- Carrinho.java
+++ Carrinho.java
@@ -1,11 +1,16 @@
-import java.util.ArrayList;
 import java.util.List;
-
+import java.util.ArrayList;
 public class Carrinho{
-    private List Produto produto;
+    private List<Produto> produtos;
+    private Produto produto;
     private double valortotal;
 
-    public Carrinho(){
-        this.produto=new ArrayList();
+    public Carrinho(List<Produto> produtos,double valortotal){
+        this.produtos=new ArrayList<>();
+        this.valortotal=valortotal;
+    }
+
+    public void addProduto(List<Produto> produtos){
+        produtos.add(produto);
     }
 }
