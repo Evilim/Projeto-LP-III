daaf42bb0431d367add2a3e0c7163c9293c59d6f
Evilim <evilimgabriela.eg@gmail.com>
2021-06-16 12:58:48 -0300

    Alteração nos atributos e construtor

--- Loja.java
+++ Loja.java
@@ -1,13 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Loja{
     private String nome;
-    private List Cliente cliente;
-    private List Produto produto;
-    private List Fornecedor fornecedor;
+    private List<Cliente> cliente;
+    private List<Produto> produto;
+    private List<Fornecedor> fornecedor;
 
-    public Loja(String nome,List Cliente cliente,List Produto produto,List Fornecedor fornecedor){
+    public Loja(String nome,List<Cliente> cliente,List<Produto> produto,List<Fornecedor> fornecedor){
         this.nome=nome;
-        this.cliente=cliente;
-        this.produto=produto;
-        this.fornecedor=fornecedor;
+        this.cliente=new ArrayList<>();
+        this.produto=new ArrayList<>();
+        this.fornecedor=new ArrayList<>();
     }
 }
